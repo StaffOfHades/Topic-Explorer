111c0b8ddc8f3f07ffdc2abdd3c2826b690b926a
Mauricio Graciano <mau.graci@gmail.com>
2021-09-26 20:16:55 -0500

    Add type-checking support at runtime

--- apps/topic-explorer/next.config.js
+++ apps/topic-explorer/next.config.js
@@ -1,5 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withNx = require('@nrwl/next/plugins/with-nx');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+/* eslint-enable @typescript-eslint/no-var-requires */
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -9,6 +11,20 @@ const nextConfig = {
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: true,
+  },
+  /**
+   * Custom Webpack Config
+   * https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+   */
+  webpack(config, options) {
+    const { dev, isServer } = options;
+
+    // Do not run type checking twice:
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+
+    return config;
   },
 };
 
